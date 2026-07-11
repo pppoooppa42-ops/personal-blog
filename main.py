6edc325c464db389f1b5aec276b73fb1dcc0f6d3
pppoooppa42-ops <pppoooppa42@gmail.com>
2026-07-11 19:51:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 random_facts = ["моя любимоя игра - CS2","я играю в cs 2 300 чясов","моя 1 игра minecraft","мою любимое блюдо это роллы","мое домашнее животное-дегу","я люблю заниматся тхеквондо итф","я люблю гулять с друзьями"]
 
+# @app.route("/")
+# def random_fact():
+#     return random.choice(random_facts)
 @app.route("/")
-def random_fact():
-    return random.choice(random_facts)
+def about_me():
+    return "я артемий мне 13 лет и я учусь програмированию на пайтон😎 <br> этот сайт сохдан с помощю flask☠️ <br> мне запомнелось ставить череп👍"
 
 app.run(debug=True)
